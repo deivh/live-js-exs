5ebd391ae5a0b1cb1e136cd2222b3df207545d0c
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-03-25 12:12:18 +0100

    js: reverse

--- js/script.js
+++ js/script.js
@@ -1,87 +1,14 @@
-// Crea 10 oggetti che rappresentano una zucchina.
-// Dividi in due array separati le zucchine che misurano
-// meno o più di 15cm.
-// Infine stampa separatamente quanto pesano i due gruppi
-// di zucchine
+// Scrivi una funzione che accetti una stringa come
+// argomento e la ritorni girata (es. Ciao -> oaiC)
 
-function arrObj() {
-    var arr15 = [];
-    var arrnot15 = [];
-  for (var i=0; i<arrZucch.length; i++ ) {
-      var zucch= arrZucch[i];
-      if (zucch.lunghezza>15) {
-          arr15.push(zucch);
-      } else {
-          arrnot15.push(zucch);
-      }
-  }
-  console.log(arr15, arrnot15);
-}
-function objCreate(boxCount) {
-    arrZucch = [
-        {
-            'varietà': 'my',
-            'peso': 15,
-            'lunghezza': 10
-        },
-        {
-            'varietà': 'my',
-            'peso': 7,
-            'lunghezza': 7
-        },
-        {
-            'varietà': 'my',
-            'peso': 12,
-            'lunghezza': 12
-        },
-        {
-            'varietà': 'my',
-            'peso': 10,
-            'lunghezza': 18
-        },
-        {
-            'varietà': 'my',
-            'peso': 12,
-            'lunghezza': 21
-        },
-        {
-            'varietà': 'my',
-            'peso': 15,
-            'lunghezza': 10
-        },
-        {
-            'varietà': 'my',
-            'peso': 7,
-            'lunghezza': 7
-        },
-        {
-            'varietà': 'my',
-            'peso': 12,
-            'lunghezza': 12
-        },
-        {
-            'varietà': 'my',
-            'peso': 10,
-            'lunghezza': 18
-        },
-        {
-            'varietà': 'my',
-            'peso': 12,
-            'lunghezza': 21
-        }
-    ]
-    
-    var sum = 0;
-    for (var i=0; i<arrZucch.length; i++){
-        sum += arrZucch[i].peso;
-    }
-    console.log(sum);
-    
+function stringRev(word) {
+    wordReverse = word.split('').reverse().join('');
+    return wordReverse; 
 }
 function init() {
-    objCreate();
-    arrObj();
- 
+    var word= prompt("Inserisci una parola");
+    wordReverse = stringRev(word);
+    console.log(wordReverse);
 }
 // JQUERY
 $(document).ready(init);
