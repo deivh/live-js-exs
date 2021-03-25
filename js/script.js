ffd7f7be1d84548e0141bb8331d076f5ecb33c1c
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-03-25 10:32:08 +0100

    js-jq: boxes

--- js/script.js
+++ js/script.js
@@ -1,81 +1,38 @@
-// GOAL: associare evento ai bottoni 
-// + e - in modo da far variare il numero al centro di +/- 1
-function randomColor() {
-    var back = ["#ff0000","blue","gray","yellow", "green" , "greenyellow", "purple" ,"pink", "violet" ];
-    var rand = back[Math.floor(Math.random() * back.length)];
-    return rand;
+function getRandomValue(min, max) {
+    var localMin = min;
+    var localMax = max - min + 1;
+    return Math.floor(Math.random() * localMax) + localMin;
 }
-
-function decreasew() {
-    var valueClick =$('#targetw').text();
-    valueClick--;
-    $('#circle').css('width', (valueClick * 10) + 'px');
-    $('#circle').css('background', randomColor)
-    
-    // $('#circle').css('height', valCircle);
-    if (valueClick<0) {
-        alert("COSA STAI FACENDO FOLLE NON ESISTE NULLA SOTTO LO ZERO");
-        valueClick = 0;
-    }
-    $("#targetw").text(valueClick);
-
-} 
-
-function increasew() {
-    var valueClick =$('#targetw').text();
-    valueClick++;
-    $('#circle').css('width', (valueClick * 10) + 'px');
-    $('#circle').css('background', randomColor)
-    // $('#circle').css('height', valCircle);
-    if (valueClick>20) {
-        alert("COSA STAI FACENDO *scemino* NON ESISTE NULLA SOPRA IL VENTI");
-        valueClick = 20;
-    }
-    $("#targetw").text(valueClick);
+function addRandomValues() {
+    $('.box').click(function() {
+        var clickedBtn = $(this);
+        var clickedSpan = clickedBtn.find('span');
+        var isSpanVisible = clickedSpan.is(':visible');
+        if (isSpanVisible) {
+            clickedBtn.remove();
+        } else {
+            var clickedValue = clickedSpan.text();
+            if (clickedValue % 2 == 0) {
+                clickedBtn.toggleClass('bg-green');
+            } else {
+                clickedBtn.toggleClass('bg-red');
+            }
+            clickedSpan.toggle();
+        }
+    });
 }
-
-function decreaseh() {
-    var valueClick =$('#targeth').text();
-    valueClick--;
-    $('#circle').css('height', (valueClick * 10) + 'px');
-    $('#circle').css('background', randomColor)
-    // $('#circle').css('height', valCircle);
-    if (valueClick < 0) {
-        alert("COSA STAI FACENDO FOLLE NON ESISTE NULLA SOTTO LO ZERO");
-        valueClick = 0;
+function boxGenerator(boxCount) {
+    var boxes = $('.boxes');
+    for (var i=0;i<boxCount;i++) {
+        var rnd = getRandomValue(1, 100);
+        boxes.append('<div class="box"><span class="text hide">' 
+            + rnd + '</span></div>');
     }
-    $("#target").text(valueClick);
-
-} 
-
-function increaseh() {
-    var valueClick =$('#targeth').text();
-    valueClick++;
-    $('#circle').css('height', (valueClick * 10) + 'px');
-    $('#circle').css('background', randomColor)
-    // $('#circle').css('height', valCircle);
-    if (valueClick>20) {
-        alert("COSA STAI FACENDO *scemino* NON ESISTE NULLA SOPRA IL VENTI");
-        valueClick = 20;
-    }
-    $("#targeth").text(valueClick);
 }
-
-
-function init(value) {
-    $('#circle').css('width', '100px');
-    $('#circle').css('height', '100px');
-    $('#minush').click(decreaseh);
-    $('#plush').click(increaseh);
-    $('#targeth').text(value);
-    $('#minusw').click(decreasew);
-    $('#plusw').click(increasew);
-    $('#targetw').text(value);
-
+function init() {
+    var boxCount = parseInt(prompt('Dimmi il numero di quadratini'));
+    boxGenerator(boxCount);
+    addRandomValues();
 }
-
-value = 10;
-init(value);
-
-
-// GOAL: ridimensionare in larghezza ed altezza il cerchio alla pressione dei tasti + e -
+// JQUERY
+$(document).ready(init);
