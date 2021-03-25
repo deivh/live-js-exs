d3d2919b16194a3a1e2cd998c8f9aeb822dae194
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-03-25 11:25:17 +0100

    js: ex 'the dawn of the zucchini"
    changed bg color

--- js/script.js
+++ js/script.js
@@ -1,16 +1,48 @@
-// Crea un array di 10 oggetti che rappresentano una
-// zucchina, indicando per ognuna varietà, peso e lunghezza.
-// Calcola quanto pesano tutte le zucchine.
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano
+// meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi
+// di zucchine
 
 function arrObj() {
-  
+    var arr15 = [];
+    var arrnot15 = [];
+  for (var i=0; i<arrZucch.length; i++ ) {
+      var zucch= arrZucch[i];
+      if (zucch.lunghezza>15) {
+          arr15.push(zucch);
+      } else {
+          arrnot15.push(zucch);
+      }
+  }
+  console.log(arr15, arrnot15);
 }
 function objCreate(boxCount) {
-    var arrZucch = [
+    arrZucch = [
         {
             'varietà': 'my',
             'peso': 15,
             'lunghezza': 10
+        },
+        {
+            'varietà': 'my',
+            'peso': 7,
+            'lunghezza': 7
+        },
+        {
+            'varietà': 'my',
+            'peso': 12,
+            'lunghezza': 12
+        },
+        {
+            'varietà': 'my',
+            'peso': 10,
+            'lunghezza': 18
+        },
+        {
+            'varietà': 'my',
+            'peso': 12,
+            'lunghezza': 21
         },
         {
             'varietà': 'my',
@@ -19,18 +51,23 @@ function objCreate(boxCount) {
         },
         {
             'varietà': 'my',
-            'peso': 15,
-            'lunghezza': 10
+            'peso': 7,
+            'lunghezza': 7
         },
         {
             'varietà': 'my',
-            'peso': 15,
-            'lunghezza': 10
+            'peso': 12,
+            'lunghezza': 12
         },
         {
             'varietà': 'my',
-            'peso': 15,
-            'lunghezza': 10
+            'peso': 10,
+            'lunghezza': 18
+        },
+        {
+            'varietà': 'my',
+            'peso': 12,
+            'lunghezza': 21
         }
     ]
     
@@ -43,6 +80,7 @@ function objCreate(boxCount) {
 }
 function init() {
     objCreate();
+    arrObj();
  
 }
 // JQUERY
